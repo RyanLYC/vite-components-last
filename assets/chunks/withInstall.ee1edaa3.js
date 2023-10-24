452c48fda7e171a97bf1131e96a3295076e92679
Ryan <656040874@qq.com>
2023-10-24 10:08:13 +0800

    'test'

--- assets/chunks/withInstall.ee1edaa3.js
+++
@@ -1,1 +0,0 @@
-function i(e,t,f){const c=e;if(c.install=n=>{for(const o of[e,...Object.values(t??{})])n.component(o.name,o);if(f)for(const o of f)n.directive(o.name,o)},t)for(const[n,o]of Object.entries(t))c[n]=o;return c}export{i as w};
